7c70276b437ee685819589bc7c1e528f92134382
Valerie <92495807+VSanto@users.noreply.github.com>
2022-03-22 19:11:44 -0500

    Retirement Info

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -38,3 +38,12 @@ select emp_no, first_name, last_name
 from employees
 where (birth_date between '1955-01-01' and '1955-12-31');
 
+-- Creating table retierment info
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Viewing table
+SELECT * FROM retirement_info;
